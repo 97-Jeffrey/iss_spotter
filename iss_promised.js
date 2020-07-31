3c0b832a97f27e81abcc295efe163e0993056c59
Jeffrey Shao <shaoyuhao970909@gmail.com>
2020-07-31 04:15:05 +0000

    refactored by promise

---
+++ iss_promised.js
@@ -0,0 +1,32 @@
+const request = require('request-promise-native');
+
+const fetchMyId = function () {
+  return request("https://api.ipify.org?format=json");
+}
+
+
+const fetchCoordsByIP = function (body) {
+  const data = JSON.parse(body).ip;
+  return request(`https://ipvigilante.com/json/${data}`);
+}
+
+const fetchISSFlyOverTimes = function (body) {
+  const { latitude, longitude } = JSON.parse(body).data;
+  const url = `http://api.open-notify.org/iss-pass.json?lat=${latitude}&lon=${longitude}`;
+  return request(url);
+}
+
+const nextISSTimesForMyLocation = function () {
+  return fetchMyId()
+    .then(fetchCoordsByIP)
+    .then(fetchISSFlyOverTimes)
+    .then((data)=>{
+      const {response} = JSON.parse(data);
+      return response;
+    })
+};
+
+
+
+module.exports = { nextISSTimesForMyLocation };
+
